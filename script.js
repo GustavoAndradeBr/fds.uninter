c02033f7dbc9dca0346d98f7b54a014538f5484d
Gustavo H.P Andrade <168314032+GustavoAndradeBr@users.noreply.github.com>
2025-05-15 00:47:54 -0300

    Update script.js

--- script.js
+++ script.js
@@ -3,6 +3,6 @@ function mudar(){
     const imagem = document.getElementById("imagemd");
 
     mensagem.textContent = "Felicidade é como uma borboleta: quanto mais você tenta apanhá-la, mais ela se afasta de você. Mas se você dirigir sua atenção para outras coisas, ela virá e pousará suavemente no seu ombro. -Davi Brito BBB24";
-    imagem.src = "images/667026534-davi-brito.jpg";
+    imagem.src = "images/davi.jpg";
     imagem.style.display = "block";
 }
